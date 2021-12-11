de921a1b90f32cb02719ef7bb1cd34896ffda325
MarcJus <jusseaumemarc@gmail.com>
2021-12-11 18:39:45 +0100

    ajout du commands.ts avec différentes commandes

---
+++ dist/commands.js
@@ -0,0 +1,30 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const opera_path = "\"C:\\Users\\jusse\\AppData\\Local\\Programs\\Opera GX\\launcher.exe\"";
+export default {
+    1: "code",
+    2: opera_path,
+    3: opera_path + " https://www.ecoledirecte.com",
+    4: "\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\" https://www.google.com/",
+    5: () => __awaiter(void 0, void 0, void 0, function* () {
+        return yield get_devoirs_platypus();
+    }),
+    6: "wt.exe ssh pi@rspm",
+    7: "wt.exe",
+    8.: 9, "\"C:\\Users\\jusse\\AppData\\Local\\Postman\\Postman.exe\"": ,
+    "*": "ipconfige",
+    0: "ipconfig",
+    "#": "",
+    A: "ipconfig",
+    B: "ipconfig",
+    C: "ipconfig",
+    D: "ipconfig",
+    "start_server": "wt.exe \"C:\\Users\\jusse\\OneDrive\\Bureau\\serveur 1.17\\start.bat\""
+};
---
+++ src/commands.ts
@@ -0,0 +1,22 @@
+const opera_path =
+"\"C:\\Users\\jusse\\AppData\\Local\\Programs\\Opera GX\\launcher.exe\"";
+
+export default {
+    1: "code",
+    2: opera_path,
+    3: opera_path + " https://www.ecoledirecte.com",
+    4: "\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\" https://www.google.com/",
+    5: "",
+    6: "wt.exe ssh pi@rspm",
+    7: "wt.exe",
+    8: "",
+    9: "\"C:\\Users\\jusse\\AppData\\Local\\Postman\\Postman.exe\"",
+    "*": "ipconfige",
+    0: "ipconfig",
+    "#": "",
+    A: "ipconfig",
+    B: "ipconfig",
+    C: "ipconfig",
+    D: "ipconfig",
+    "start_server": "wt.exe \"C:\\Users\\jusse\\OneDrive\\Bureau\\serveur 1.17\\start.bat\""
+};
