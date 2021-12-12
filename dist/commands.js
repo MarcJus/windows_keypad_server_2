d6c9243b3d9fbe1c37343b2651698013dbae94f2
MarcJus <jusseaumemarc@gmail.com>
2021-12-12 12:15:32 +0100

    tsc

--- dist/commands.js
+++ dist/commands.js
@@ -1,24 +1,14 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 const opera_path = "\"C:\\Users\\jusse\\AppData\\Local\\Programs\\Opera GX\\launcher.exe\"";
 export default {
     1: "code",
     2: opera_path,
     3: opera_path + " https://www.ecoledirecte.com",
     4: "\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\" https://www.google.com/",
-    5: () => __awaiter(void 0, void 0, void 0, function* () {
-        return yield get_devoirs_platypus();
-    }),
+    5: "",
     6: "wt.exe ssh pi@rspm",
     7: "wt.exe",
-    8.: 9, "\"C:\\Users\\jusse\\AppData\\Local\\Postman\\Postman.exe\"": ,
+    8: "",
+    9: "\"C:\\Users\\jusse\\AppData\\Local\\Postman\\Postman.exe\"",
     "*": "ipconfige",
     0: "ipconfig",
     "#": "",
--- dist/process.js
+++ dist/process.js
@@ -7,12 +7,62 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+import { exec } from "child_process";
+import commands from "./commands";
 export function executeKey(key) {
     return __awaiter(this, void 0, void 0, function* () {
         let result = {
             error: false,
             message: ""
         };
+        const command = commands[key];
+        if (typeof command === "string") {
+            try {
+                result = yield executeCommand(command);
+            }
+            catch (e) {
+                console.log(e);
+            }
+        }
+        else {
+            command();
+        }
         return result;
+    });
+}
+function executeCommand(command) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve, reject) => {
+            let command_exec_information = { error: false, message: "" };
+            try {
+                exec(command, (error, stdout, stderr) => {
+                    if (error != null) {
+                        command_exec_information = {
+                            error: true,
+                            message: error.message
+                        };
+                        reject(error);
+                    }
+                    if (stderr !== "") {
+                        command_exec_information = {
+                            error: true,
+                            message: stderr
+                        };
+                    }
+                    else {
+                        command_exec_information = {
+                            error: false,
+                            message: stdout
+                        };
+                    }
+                    resolve(command_exec_information);
+                });
+            }
+            catch (e) {
+                console.log("catch");
+                reject(e);
+            }
+            resolve(command_exec_information);
+        });
     });
 }
