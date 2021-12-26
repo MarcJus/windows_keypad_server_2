46a9df035a7c09343eb0466f1f2b817ace9fec65
MarcJus <jusseaumemarc@gmail.com>
2021-12-26 15:52:41 +0100

    ajout de console.log

--- src/process.ts
+++ src/process.ts
@@ -20,6 +20,11 @@ interface CommandExecInformation {
 
 }
 
+/**
+ * * Fonction executée en premier
+ * @param key Touche entrée
+ * @returns Informations sur le retour de la commande/fonction executée ou null en cas d'erreur
+ */
 export async function executeKey(key: Key): Promise<CommandExecInformation | null>{
     let result: CommandExecInformation = {
         error: false,
@@ -30,22 +35,30 @@ export async function executeKey(key: Key): Promise<CommandExecInformation | nul
         try{
             result = await executeCommand(command);
         } catch (e) {
-            console.log(e);
+            console.log("error executeKey");
         }
     } else {
-        (command as () => void)();
+        (command as () => void)(); // TODO Gérer les erreurs
     }
-
+    console.log("result : "+result);
     return result;
 }
 
+/**
+ * ! Executée après executeCommand
+ * @param command Commande à executer
+ * @returns Informations sur le retour de la commande
+ */
 async function executeCommand(command: string): Promise<CommandExecInformation>{
+    console.log(command);
     return new Promise((resolve, reject) => {
         let command_exec_information: CommandExecInformation = {error: false, message: ""};
 
         try{
             exec(command, (error: ExecException | null, stdout: string, stderr: string) => {
+                console.log("error : "+error+", stdout : "+stdout+", stderr : "+stderr);
                 if(error != null){
+                    console.log("error != null");
                     command_exec_information = {
                         error: true,
                         message: error.message
@@ -63,6 +76,7 @@ async function executeCommand(command: string): Promise<CommandExecInformation>{
                         message: stdout
                     };
                 }
+                console.log("resolve");
                 resolve(command_exec_information);
             });
         } catch (e) {
