517b98b4f6da33d4e2f6aff844ef4213e016d303
MarcJus <jusseaumemarc@gmail.com>
2021-12-26 15:53:20 +0100

    try catch de l'execution de la touche entrée

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import net, {Server} from "net";
+import { executeKey, Key } from "./process";
 
 const server: Server = net.createServer();
 const port = 3000;
@@ -19,7 +20,13 @@ server.on("connection", (socket: net.Socket) => {
         const string_data: string = data.toString("utf-8");
         console.log(`Message de ${clientAddress} : ${string_data}`);
 
-        
+        try {
+            const command_exec_information = await executeKey((string_data as Key));
+            console.log("command exec information : ",command_exec_information);
+        } catch (e) {
+            console.log(e);
+            socket.write("error");
+        }
 
         // Réponse : succes
         socket.write("success");
