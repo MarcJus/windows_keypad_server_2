cafcfcc414cfeb69b08d897341fdf888e5bfdf53
MarcJus <jusseaumemarc@gmail.com>
2022-01-21 17:21:25 +0100

    creation du type ListenerFunction et du listeners_list

--- src/modes/modes.ts
+++ src/modes/modes.ts
@@ -3,4 +3,10 @@
  */
 export type NumberKey = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0";
 
+type ListenerFunction = (...args: string[]) => void
 
+
+
+const listeners_list: Record<string, ListenerFunction> = {};
+
+export default listeners_list;
